ce7f842a048c07116d0d55d8a369466bd4f83ad1
igorcarmona-git <igorcarmonawork@gmail.com>
2024-08-12 19:21:13 -0300

    fix: funcioanndo

--- index.js
+++ index.js
@@ -3,8 +3,27 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+
+
 // Servir arquivos estáticos da pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json()); 
+
+app.post('/message', (req, res) => {
+  const userMessage = req.body.message;
+
+  // Simples lógica de resposta
+  let botResponse = 'Desculpe, não entendi.';
+
+  if (userMessage.includes('oi') || userMessage.includes('olá')) {
+    res.sendFile(path.join(__dirname, 'public', 'index-Hello.html'));
+
+  } else if (userMessage.includes('adeus') || userMessage.includes('tchau')) {
+    res.sendFile(path.join(__dirname, 'public', 'index-Bye.html'));
+
+  }
+
+});
 
 // Rota para a página principal
 app.get('/', (req, res) => {
